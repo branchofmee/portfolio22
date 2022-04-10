5ec3a37f9d536c467c5c245a74638f111f9f4725
mee <branchofmee@gmail.com>
2022-04-10 22:23:41 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import { Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
+import portfolioData from './data.js';
+import './App.css';
+
+
+function App() {
+
+  let [menu2, menu2change] = useState(false);
+  let [portfolio, portfolioChange] = useState(portfolioData);
+
+  return (
+    <div className="App">
+      <div className="leftbox">
+        <div className="leftbox-0">
+          <div className="leftbox-1">
+            <img src={require('./img/pic.jpg')} alt="photo" />
+          </div>
+          <div className="leftbox-2">
+            <p className="fontstyle1">배미경<br /><span> Bae Mee Gyung</span></p>
+            <div className="leftbox2-1">
+              <p className="fontstyle2">BIRTH</p>
+              <p className="fontstyle3">1990.05.02</p>
+              <p className="fontstyle2">E-MAIL</p>
+              <p className="fontstyle3">branchofmee@gmail.com</p>
+              <p className="fontstyle2">EDUCATION</p>
+              <p className="fontstyle3">숙명여자대학교 문헌정보학과 졸업</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="rightbox">
+        <Nav
+          activeKey="/home"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+          <Nav.Item>
+            <button onClick={() => { menu2change(false) }}><i class="fa-solid fa-code"></i>About Me</button>
+          </Nav.Item>
+          <Nav.Item>
+            <button onClick={() => { menu2change(true) }}><i class="fa-solid fa-floppy-disk"></i>Work</button>
+          </Nav.Item>
+          <Nav.Item><a href={`https://github.com/branchofmee/portfolio22`} target='_blank' className="nav-a"><i class="fab fa-github"></i>Go portfolio github</a></Nav.Item>
+          <Nav.Item><a href={`https://github.com/branchofmee/portfolio`} target='_blank' className="nav-a"><i class="fab fa-github"></i>Go project github</a></Nav.Item>
+        </Nav>
+
+        <div className="rightbox-1">
+          <div className="innerwrap">
+            <div className="rightbox1-1">
+
+              <div className="rb1-1-1-1">
+                <div className="rb1-1-title">
+                  <h5>About Me</h5>
+                </div>
+                <div className="rb1-1-textbox">
+                  <p className="fontstyle4">
+                    Hi!<br />
+                    My name is<br />
+                    BAE MeeGyung<br />
+                    I'm Getting Better All The Time!
+                  </p>
+                </div>
+              </div>
+
+
+              <div className="rb1-1-1-1">
+                <div className="rb1-1-title">
+                  <h5>LICENCE</h5>
+                </div>
+                <div className="rb1-1-textbox">
+                  <p className="fontstyle4">웹디자인 기능사(필기 합격)</p>
+                </div>
+              </div>
+
+              <div className="rb1-1-1-1">
+                <div className="rb1-1-title">
+                  <h5>TRAINING</h5>
+                </div>
+                <div className="rb1-1-textbox">
+                  <p className="fontstyle4">이젠컴퓨터학원 웹퍼블리셔 UI/UX 프론트엔드 양성과정 수료(2022년 3월)</p>
+                </div>
+              </div>
+
+              <div className="rb1-1-1-1">
+                <div className="rb1-1-title">
+                  <h5>SKILL</h5>
+                </div>
+                <div className="rb1-1-textbox">
+                  <img src={require('./img/html.png')} alt="skillicon" />
+                  <img src={require('./img/css.png')} alt="skillicon" />
+                  <img src={require('./img/js.png')} alt="skillicon" />
+                  <img src={require('./img/jquery.png')} alt="skillicon" />
+                  <img src={require('./img/json.png')} alt="skillicon" />
+                  <img src={require('./img/ajax.png')} alt="skillicon" />
+                  <img src={require('./img/react.png')} alt="skillicon" />
+                  <img src={require('./img/bootstrap.png')} alt="skillicon" />
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+          {
+            menu2 === true
+              ? <div className="rightbox1-2">
+                <div className="innerwrap">
+              {
+                portfolio.map((a, i)=>{
+                  return <Rightbox121 portfolio={a} i={i} key={i}/>
+                })
+              }
+                </div>  
+              </div>
+              : null
+          }
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+
+function Rightbox121(props) {
+  return (
+    <div className="rightbox1-2-1">
+      <img src={require('./img/portfolio' + (props.i + 1) + '.png')} alt="img" />
+      <div>
+        <h3>
+          {props.portfolio.title}
+        </h3>
+
+        <p className='fontstyle5'>
+          {props.portfolio.skill}
+        </p>
+        <p className='fontstyle5'>
+          {props.portfolio.skill2}
+        </p>
+
+        <p className='fontstyle6' >
+          {props.portfolio.content1}<br />
+          {props.portfolio.content2}<br />
+          {props.portfolio.content3}
+        </p>
+
+      </div>
+    </div>
+  )
+}
+export default App;
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    id : 0,
+    title:"포트폴리오 웹",
+    skill: "React",
+    skill2: "Bootstrap",
+    content1 : "react를 공부하면서 만든 개인 포트폴리오 웹앱 입니다.",
+    content2 : "Nav는 Bootstrap을 이용하여 만들었습니다.",
+    content3 : "component와 map을 사용하여 코드를 경제적으로 짜는데 중점을 두며 제작하였습니다."
+  },
+  {
+    id:1,
+    title:"밀리의 서재",
+    skill: "Open API",
+    skill2: "Responsive Website",
+    content1 : "국내 도서 사이트인 밀리의 서재의 클론 사이트입니다.",
+    content2 : "페이지 내의 도서 이미지 및 제목은 Kakao에서 제공하는 Open Api 데이터를 받아 사용하였습니다. 미디어를 이용하여 다양한 화면에서의 대응할 수 있도록 하였고, jquery를 사용하여 스크롤과 클릭에 반응하도록 작업하였습니다.",
+    content3 : "jquery의 사용에 있어서는 비슷한 결과물이라도 반복문을 이용하여 클릭에 대응해보는 식으로 여러방식의 접근을 시도 해보았습니다."
+  },
+  {
+    id:2,
+    title:"Confer",
+    skill: "Jquery",
+    skill2: "Responsive Website",
+    content1 : "해외 스피치 콘테스트 CONFER의 홍보 웹사이트입니다.",
+    content2 : "세번째 작업물로 이전의 두 작업물과 UI에 차별점을 두어 어두운 배경에 그라데이션과 채도가 높은 색을 사용하여 주목성을 높였습니다.",
+    content3 : "또한 사이트 방문자의 편의와 시각적 만족감을 위하여 버튼에 마우스를 올렸을 때 새로운 그라데이션이 나오게 하는 등 다양한 반응형 요소를 삽입하였습니다."
+  },
+  {
+    id:3,
+    title:"Siamo Napoli",
+    skill: "Jquery",
+    skill2: "Responsive Website",
+    content1 : "캘리포니아 남서부에 위치한 파인 다이닝 레스토랑 Siamo Napoli의 클론 사이트입니다.",
+    content2 : "두번째로 만든 작업물로 이 작업물부터 상황에 맞지않는 code의 사용을 없애고, float을 원래 용도인 이미지와 텍스트의 배열을 위한 용도로만 사용하는 등 적절한 코드 사용을 하기 위해 주의를 기울였습니다.",
+    content3 : "아울러 media를 사용하여 다양한 화면에 대응할 수 있도록 하였습니다"
+  },
+  {
+    id:4,
+    title:"LeWeekend",
+    skill: "Json",
+    skill2: "Jquery",
+    content1 : "국내 인테리어 쇼핑몰인 Le Weekend의 클론 사이트입니다.",
+    content2 : "첫 작업물로 이후 json을 이용하여 제품 이미지와 정보 데이터를 받아올 수 있도록 수정하였습니다.",
+    content3 : "방문자가 상품에 집중할 수 있도록 도우면서 제품의 고급스러운 이미지를 부각하기 위하여 효과와 색을 절제하여 사용하였습니다."
+  }
+]
